ea861a09deb5f08919cac4a53f21019570cdd14a
tyuan <tyuan@student.42.fr>
2022-05-09 10:27:18 +0200

    bonus done

--- vector/set.hpp
+++ vector/set.hpp
@@ -23,16 +23,16 @@ namespace ft {
 		typedef size_t		size_type;
 		typedef ptrdiff_t	difference_type;
 
-		typedef typename allocator_type::reference			reference;
+		typedef typename allocator_type::const_reference	reference;
 		typedef typename allocator_type::const_reference	const_reference;
-		typedef typename allocator_type::pointer			pointer;
+		typedef typename allocator_type::const_pointer		pointer;
 		typedef typename allocator_type::const_pointer		const_pointer;
 
 		typedef ft::RBtree<value_type, value_compare>		tree_type;
-		typedef typename tree_type::iterator				iterator;
 		typedef typename tree_type::const_iterator			const_iterator;
-	//	typedef const_iterator								iterator;
-		typedef ft::reverse_iterator<iterator>				reverse_iterator;
+		typedef typename tree_type::const_iterator			iterator;
+		typedef	typename tree_type::iterator				treeIt;
+		typedef ft::reverse_iterator<const_iterator>		reverse_iterator;
 		typedef ft::reverse_iterator<const_iterator>		const_reverse_iterator;
 
 	//canonical
@@ -134,12 +134,13 @@ namespace ft {
 	//modifiers
 		ft::pair<iterator, bool>	insert(const value_type& x)
 		{
-			return (this->_tree.insert(x));
+			ft::pair<treeIt, bool> ret = this->_tree.insert(x);
+			return (ft::pair<iterator, bool>(ret.first, ret.second));
 		}
 
 		iterator insert(iterator position, const value_type& x)
 		{
-			return (this->_tree.insert(position, x));
+			return (this->_tree.insert((treeIt &)position, x));
 		}
 
 		template <class InputIterator>
@@ -150,7 +151,7 @@ namespace ft {
 
 		void erase(iterator position)
 		{
-			this->_tree.erase(position);
+			this->_tree.erase((treeIt &)position);
 		}
 
 		size_type erase(const key_type& x)
@@ -160,7 +161,7 @@ namespace ft {
 
 		void erase(iterator first, iterator last)
 		{
-			this->_tree.erase(first, last);
+			this->_tree.erase((treeIt &)first, (treeIt &)last);
 		}
 
 		void swap(set& rhs)
@@ -179,12 +180,7 @@ namespace ft {
 		value_compare value_comp() const { return (value_compare()); }
 
 	//set operations
-		iterator		find(const key_type& x)
-		{
-			return (this->_tree.find(x));
-		}
-
-		const_iterator	find(const key_type& x) const
+		iterator		find(const key_type& x) const
 		{
 			return (this->_tree.find(x));
 		}
@@ -194,7 +190,7 @@ namespace ft {
 			return (this->find(x) != this->end());
 		}
 	
-		iterator	lower_bound(const key_type& x)
+		iterator	lower_bound(const key_type& x) const
 		{
 			iterator	begin = this->begin();
 			iterator	end = this->end();
@@ -208,21 +204,7 @@ namespace ft {
 			return (begin);
 		}
 
-		const_iterator	lower_bound(const key_type& x) const
-		{
-			const_iterator	begin = this->begin();
-			const_iterator	end = this->end();
-	
-			while (begin != end)
-			{
-				if (!(_comp(*begin, x)))
-					return (begin);
-				begin++;
-			}
-			return (begin);
-		}
-
-		iterator		upper_bound(const key_type& x)
+		iterator		upper_bound(const key_type& x) const
 		{
 			iterator	begin = this->begin();
 			iterator	end = this->end();
@@ -236,26 +218,7 @@ namespace ft {
 			return (begin);
 		}
 
-		const_iterator		upper_bound(const key_type& x) const
-		{
-			const_iterator	begin = this->begin();
-			const_iterator	end = this->end();
-	
-			while (begin != end)
-			{
-				if (_comp(x, *begin))
-					return (begin);
-				begin++;
-			}
-			return (begin);
-		}
-
-		pair<iterator, iterator>	equal_range(const key_type& x)
-		{
-			return (ft::make_pair(this->lower_bound(x), this->upper_bound(x)));
-		}
-
-		pair<const_iterator, const_iterator>	equal_range(const key_type& x) const
+		pair<iterator, iterator>	equal_range(const key_type& x) const
 		{
 			return (ft::make_pair(this->lower_bound(x), this->upper_bound(x)));
 		}
--- vector/tools.hpp
+++ vector/tools.hpp
@@ -118,7 +118,7 @@ namespace ft
 		pair() : first(), second() {}
 
 		template<class U, class V>
-		pair (const pair<U,V>& pr) : first(pr.first), second(pr.second) {}
+		pair (const pair<U,V>& rhs) : first(rhs.first), second(rhs.second) {}
 
 		pair (const first_type& a, const second_type& b) : first(a), second(b) {}
 
